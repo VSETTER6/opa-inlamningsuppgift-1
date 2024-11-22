c6caaded947d6053ff030df21ca9671235b5600b
VSETTER6 <vilhelm.settergren@live.se>
2024-11-22 18:13:47 +0100

    BookModel and AuthorModel added to Domain

---
+++ opa-inlamningsuppgift-1/Domain/Models/BookModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public BookModel(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+    }
+}
